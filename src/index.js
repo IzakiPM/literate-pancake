66d3731660a25331f893c3d3888f0ca6e868a060
Izahn <izahnpaulmiru1@gmail.com>
2023-04-03 13:55:24 +1200

    Deleted some files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-for (let a = 1; a < 10; a++){
-    console.log(a)
-}
+// for (let a = 1; a < 21; a++){
+//     console.log(a)
+// } // This is a for loop
+
+
+
+let randomNumbers = [5, 3, 15, 20];
+
+for (let n of randomNumbers) 
+console.log(n) // This is a for in loop, suitable for arrays
