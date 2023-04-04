c6f5694887d12ce087758fb67d9d8111b6bb1646
Izahn <izahnpaulmiru1@gmail.com>
2023-04-04 21:48:49 +1200

    added another file, for SDV503  moodle ativities

---
+++ src/moodleActivities.js
@@ -0,0 +1,37 @@
+
+// // function declaration
+
+// function greeting() {
+//     console.log("Kia ora!") // this is your code block (your code goes between the curly brackets)
+// }
+
+// greeting() // This code runs or invokes the function 
+
+// function nameOfFunction() {
+//     console.log("Your function")
+// }
+
+// function typeOfFunction() {
+//     console.log("Your function again")
+// }
+
+// nameOfFunction()
+// typeOfFunction()
+
+
+
+// function expression and other bits
+
+// const shortAboutMe = function(name = "(insert first name)", occupation = "(enter occupation title).", age = "(enter age)"){
+//     console.log(`My name is ${name} I am ${age} and my current occupation is a ${occupation}`)
+// }
+
+
+// console.log("Applicant 1")
+// shortAboutMe()
+
+// console.log("\nApplicant 2")
+// shortAboutMe("Hank", "carpenter.", "38")
+
+// console.log("\nApplicant 3")
+// shortAboutMe("Grace", "plumber.", "24")
